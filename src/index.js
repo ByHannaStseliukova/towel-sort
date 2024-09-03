2abd9597cbc8dc2ca2383fb29d54abf186c9848e
Hanna Stseliukova <70797548+ByHannaStseliukova@users.noreply.github.com>
2024-09-03 22:12:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 module.exports = function towelSort (matrix) {
-  if (!matrix || matrix.length === 0){
-    return [];
+  if (matrix === undefined  || matrix.length === 0){
+    return new Array;
   } 
     let output = new Array;
-        for(let i = 0; i < matrix.length; i++) {  
+    let n = matrix.length;
+        for(let i = 0; i < n; i++) {  
             if ((i+1) % 2 === 0) {
                 matrix[i] = matrix[i].reverse();
             } 
